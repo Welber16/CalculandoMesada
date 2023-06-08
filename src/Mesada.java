ee15df051f36db9dee47d6ddb52c8db11a095f3c
welber16 <binho.3w@gmail.com>
2023-06-08 12:33:27 -0300

    first commit

---
+++ src/Mesada.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Mesada {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int myString = 45;
+        int myInt = 100;
+        scanner.close();
+
+        System.out.println("myString is: " + myString);
+        System.out.println("myInt is: " + myInt);
+
+
+    }
+}
